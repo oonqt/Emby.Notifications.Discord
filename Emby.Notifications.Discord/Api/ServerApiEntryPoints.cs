7232a8ad7afcaeb7a0441c51f3526502882a5d21
oonqt <r0blox1053@outlook.com>
2020-02-12 18:52:04 -0800

    Embeds test

--- Emby.Notifications.Discord/Api/ServerApiEntryPoints.cs
+++ Emby.Notifications.Discord/Api/ServerApiEntryPoints.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using MediaBrowser.Common.Net;
 using MediaBrowser.Model.Logging;
 using MediaBrowser.Model.Services;
 using Emby.Notifications.Discord.Configuration;
 using System.Threading.Tasks;
+using MediaBrowser.Model.Serialization;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Emby.Notifications.Discord.Api
 {
@@ -18,13 +20,15 @@ namespace Emby.Notifications.Discord.Api
 
     class ServerApiEndpoints : IService
     {
-        private readonly IHttpClient _httpClient;
+        private readonly HttpClient _httpClient;
         private readonly ILogger _logger;
+        private readonly IJsonSerializer _jsonSerializer;
 
-        public ServerApiEndpoints(ILogManager logManager, IHttpClient httpClient)
+        public ServerApiEndpoints(ILogManager logManager, IJsonSerializer jsonSerializer)
         {
             _logger = logManager.GetLogger(GetType().Name);
-            _httpClient = httpClient;
+            _httpClient = new HttpClient();
+            _jsonSerializer = jsonSerializer;
         }
         private DiscordOptions GetOptions(String userID)
         {
@@ -38,26 +42,52 @@ namespace Emby.Notifications.Discord.Api
             Task.WaitAll(task);
         }
 
+        public class DiscordEmbed
+        {
+            public int color { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+        }
+
+        public class DiscordMessage
+        {
+            public List<DiscordEmbed> embeds { get; set; }
+            public string username { get; set; }
+            public string avatar_url { get; set; }
+        }
+
         public async Task PostAsync(TestNotification request)
         {
             var options = GetOptions(request.UserID);
 
-            var parameters = new Dictionary<string, string> { };
-            parameters.Add("content", "This is a test notification from Emby");
-            parameters.Add("username", options.Username);
-            parameters.Add("avatar_url", options.AvatarUrl);
-             
+            var postData = new StringContent(_jsonSerializer.SerializeToString(new DiscordMessage()
+            {
+                avatar_url = options.AvatarUrl,
+                username = options.Username,
+                embeds = new List<DiscordEmbed>()
+                {
+                    new DiscordEmbed()
+                    {
+                        color = 181818,
+                        description = "That's RIGHT",
+                        title = "HA"
+                    }
+                }
+            }).ToString());
+
             _logger.Debug("Discord Request to: {0} From: {1}", options.DiscordWebhookURI, request.UserID);
 
-            var httpRequestOptions = new HttpRequestOptions { };
-
-            httpRequestOptions.Url = options.DiscordWebhookURI;
-            httpRequestOptions.RequestHeaders["Content-Type"] = "application/json";
-            httpRequestOptions.SetPostData(parameters);
-
-            using (await _httpClient.Post(httpRequestOptions).ConfigureAwait(false))
+            try
             {
+                var RequestMessage = new HttpRequestMessage(HttpMethod.Post, options.DiscordWebhookURI);
+                RequestMessage.Content = postData;
+                RequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
+                await _httpClient.SendAsync(RequestMessage).ConfigureAwait(false);
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.Error("Failed to make request to Discord: {0}", e);
             }
         }
     }
