422a6e645ec99ed93a7a389fcb1a46435b9e7117
oonqt <r0blox1053@outlook.com>
2020-03-28 13:49:30 -0700

    add message queue interval

--- Emby.Notifications.Discord/Constants.cs
+++ Emby.Notifications.Discord/Constants.cs
@@ -5,5 +5,6 @@
         public static readonly string[] AllowedMediaTypes = new string[] { "Movie", "Episode", "Audio" };
         public static readonly int RecheckIntervalMS = 10000;
         public static readonly int MaxRetriesBeforeFallback = 10;
+        public static readonly int MessageQueueSendInterval = 1000;
     }
 }
